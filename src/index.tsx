d344db2676f401f0f6939b221304a8b14f8310f7
fatih soymaz <fatihsoymaz@fatih-MacBook-Air.local>
2024-03-14 22:35:14 +0300

    .

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -19,9 +18,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <Provider store={store}>
-        <HashRouter>
+        <BrowserRouter>
           <App />
-        </HashRouter>
+        </BrowserRouter>
         </Provider>
 );
 
